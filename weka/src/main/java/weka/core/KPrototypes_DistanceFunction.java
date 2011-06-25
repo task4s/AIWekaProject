cea2166b0928b6dc4cbc3cba73b7e2ae50b59b3f
Anton Cholakov <dragonknightrex@gmail.com>
2011-06-25 22:46:43 +0300

    Added the gamma parameter to the KPrototype_DistanceFunction

--- weka/src/main/java/weka/core/KPrototypes_DistanceFunction.java
+++ weka/src/main/java/weka/core/KPrototypes_DistanceFunction.java
@@ -13,23 +13,23 @@ import java.util.Vector;
 public class KPrototypes_DistanceFunction extends NormalizableDistance
   implements Cloneable  {
 
-  private double gamma;
+  private double m_Gamma;
 
     /** for serialization. */
   private static final long serialVersionUID = 1068606253458807903L;
 
   public KPrototypes_DistanceFunction( double gamma){
-        this.gamma = gamma;
+        m_Gamma = gamma;
   }
 
   public KPrototypes_DistanceFunction() {
     super();
-    this.gamma = 0.5;
+    m_Gamma = 0.5;
   }
 
   public KPrototypes_DistanceFunction(Instances data, double gamma) {
     super(data);
-    this.gamma = gamma;
+    m_Gamma = gamma;
   }
 
   /**
@@ -71,44 +71,75 @@ public class KPrototypes_DistanceFunction extends NormalizableDistance
         }
       }
      
-      return sum_continuous + gamma*sum_nominal;
+      return sum_continuous + m_Gamma * sum_nominal;
   }
-public String[] getOptions() {
+
+  /**
+   * Returns the tip text for this property
+   * @return tip text for this property suitable for
+   * displaying in the explorer/experimenter gui
+   */
+  public String gamma() {
+    return "gamma parameter";
+  }
+
+  /**
+   * fet the gamma parameter
+   *
+   * @return the gamma parameter
+   */
+  public double getGamma()
+  {
+    return m_Gamma;
+  }
+
+  /**
+   * set the gamma parameter
+   *
+   * @param g the gamma parameter
+   */
+  public void setGamma(double g)
+  {
+    m_Gamma = g;
+  }
+
+  /**
+   * Gets the current settings of BisectingKMeans
+   *
+   * @return an array of strings suitable for passing to setOptions()
+   */
+  @Override
+  public String[] getOptions() {
     Vector<String>	result;
+    String[]            options;
     
     result = new Vector<String>();
 
-    if (getDontNormalize())
-      result.add("-D");
-    
-    result.add("-R");
-    result.add(getAttributeIndices());
-    
-    if (getInvertSelection())
-      result.add("-V");
-
     result.add("-G");
+    result.add("" + getGamma());
+
+    options = super.getOptions();
+    for (int i = 0; i < options.length; i++)
+      result.add(options[i]);
+
     //result.
     return result.toArray(new String[result.size()]);
   }
 
+  /**
+   * Parses a given list of options.
+   *
+   * @param options 	the list of options as an array of strings
+   * @throws Exception 	if an option is not supported
+   */
+  @Override
   public void setOptions(String[] options) throws Exception {
-    String	tmpStr;
+    String optionString;
 
-    setDontNormalize(Utils.getFlag('D', options));
+    optionString = Utils.getOption("G", options);
+    setGamma(Double.parseDouble(optionString));
 
-    tmpStr = Utils.getOption('R', options);
-    if (tmpStr.length() != 0)
-      setAttributeIndices(tmpStr);
-    else
-      setAttributeIndices("first-last");
-
-    tmpStr = Utils.getOption('G', options);
-    if (tmpStr.length()> 0){
-        gamma = Integer.parseInt(tmpStr);
-    }
-    
-    setInvertSelection(Utils.getFlag('V', options));
+    super.setOptions(options);
   }
 
 
